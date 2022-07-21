1b8e148ef313cdd12619165a6b313b4cfb7d3f79
salnish <salnish2016@gmail.com>
2022-07-21 13:54:58 +0530

    test

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb://127.0.0.1:27017/stickit", {
     useNewUrlParser: true
 }).then(() => {
-    console.log('connection Successfull')
+    console.log('connection Successfull okkk')
 }).catch((e) => {
     console.log('No Connection');
     console.log(e);
